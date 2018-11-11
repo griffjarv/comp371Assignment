276c68cf72735eb9805f03c8544f0f55edf209ca
DevilLordHarsh <devillordharsh@gmail.com>
2018-11-11 15:57:48 -0800

    hopefully complete

--- src/Game.java
+++ src/Game.java
@@ -10,36 +10,30 @@ import java.util.HashMap;
  */
 public class Game {
 
-//    Constants
-    private static final int SQUARE_MAP_GRID_SIZE = 10;
-    private static final int NUM_OF_TANKS = 5;
-    private static final int TANK_CELLS = 4;
-    private static final int INITIAL_FORTRESS_STRENGTH = 1500;
-
 //    Following characters represent state of each cell on Map
-    private static final char EMPTY = ' ';
-    private static final char FOG = '~';
-    private static final char SHOT_A_TANK = 'X';
-    private static final char IS_TANK = 'T';
-    private static final char MISSED_A_SHOT = '.';
+    public static final String EMPTY = " ";
+    public static final String FOG = "~";
+    public static final String SHOT_A_TANK = "X";
+    public static final String IS_TANK = "T";
+    public static final String MISSED_A_SHOT = ".";
 
-//    Variables
-    private Map gameMap;    // a map keeping track of where shots were by user
-    private Map tanksMap;   // a map only knowing tank locations
+    //    Variables
+    private Grid gameMap;    // a map keeping track of where shots were by user
     private Fortress fortress;
-
     private ArrayList<Tank> siegeTanks;
 
     private boolean playerTurn;
     private boolean gameFinished;
+    private boolean playerWin;
 
 //    Constructor
     Game(int fortressStrength, int gridSize) {
         this.gameFinished = false; // Can't be true when game just started
+        this.playerWin = false;
         this.playerTurn = true;    // Player is given the first turn
 
-        this.fortress = new Fortress(INITIAL_FORTRESS_STRENGTH);
-        this.gameMap = new Map(gridSize);
+        this.fortress = new Fortress(fortressStrength);
+        this.gameMap = new Grid(gridSize);
         this.siegeTanks = new ArrayList<>();
     }
 
@@ -71,6 +65,22 @@ public class Game {
         this.gameFinished = gameFinished;
     }
 
+    public boolean isPlayerWin() {
+        return playerWin;
+    }
+
+    public Grid getGameMap() {
+        return gameMap;
+    }
+
+    public Grid getTanksMap() {
+        return tanksMap;
+    }
+
+    public int getFortressStrength() {
+        return fortress.getStrength();
+    }
+
     public boolean makePlayerMove(Point coords) {
         boolean hit = false;
 
@@ -88,12 +98,16 @@ public class Game {
 
                             if (siegeTanks.isEmpty()) {
                                 gameFinished = true;
+                                playerWin = true;
                             }
                         }
 
                         gameMap.setCharAt(coords.x, coords.y, SHOT_A_TANK);
                         playerTurn = false;
                         return hit;
+                    }
+                    else {
+                        gameMap.setCharAt(coords.x, coords.y, MISSED_A_SHOT);
                     }
                 }
             }
@@ -112,6 +126,7 @@ public class Game {
 
                 if (siegeTanks.isEmpty()) {
                     gameFinished = true;
+                    playerWin = true;
                     return damage;
                 }
 
@@ -122,6 +137,11 @@ public class Game {
                     else {
                         siegeTanks.remove(tank);
                     }
+                }
+
+                if (siegeTanks.isEmpty()) {
+                    gameFinished = true;
+                    playerWin = true;
                 }
 
                 fortress.subtractStrength(damage);
@@ -135,9 +155,20 @@ public class Game {
         return damage;
     }
 
-    public Map finalMap() {
+    public Grid getFinalMap() {
         for (int i=0; i<gameMap.size(); i++) {
             for (int j=0; j<gameMap.size(); j++) {
+
+                boolean isTank = false;
+                for (Tank t : siegeTanks) {
+                    ArrayList<Point> c = t.getCoords();
+                    for (Point p : c) {
+                        if (p.equals(new Point(i, j))) {
+                            isTank = true;
+                        }
+                    }
+                }
+
                 // Order of following statements matters!
                 if (Character.compare(gameMap.getCharAt(i, j), SHOT_A_TANK) == 0) {
                     continue;
@@ -145,7 +176,7 @@ public class Game {
                 else if (Character.compare(gameMap.getCharAt(i, j), MISSED_A_SHOT) == 0) {
                     continue;
                 }
-                else if (Character.compare(tanksMap.getCharAt(i, j), IS_TANK) == 0) {
+                else if (isTank) {
                     gameMap.setCharAt(i, j, IS_TANK);
                 }
                 else if (Character.compare(gameMap.getCharAt(i, j), FOG) == 0) {
